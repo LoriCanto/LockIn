6dbeab22ddfa167314e66caa5b4050f505293bf4
LoriCanto <l@malg.it>
2025-12-03 12:27:33 +0100

    add page choose Loker

---
+++ lockerRoom.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8";>
+    <title>LockIn: prenota il tuo amradietto!</title>
+    <meta name="viewport" content="width=device-width,initial-scale=    1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="LockInStyle.css">
+    <link rel="stylesheet" href="lokerRoom_Style.css">
+</head>
+<body>
+    <h1>Scegli la dimensione che desideri</h1>
+    <div id='griglia'>   
+        <div id='bigLock_container'>
+            <div id='bigLock_button'>
+                <img src="images/armadiettogrande.jpg">
+                <h2>Armadietto Grande</h2>
+            </div>
+        </div>
+        <div id='smalLock_container'>
+            <div id="smalLock_button">
+                <img src="images/armadiettopiccolo.jpg">
+                <h2>Armadietto Piccolo</h2>
+            </div>
+        </div>
+    <
+</body>
