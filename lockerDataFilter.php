b501b016e379e10945fd0efa68c51b9b351fe23f
LoriCanto <l@malg.it>
2025-12-27 16:09:56 +0100

    finale ( v.1 sistemarestile e conferma)

--- lockerDataFilter.php
+++ lockerDataFilter.php
@@ -1,7 +1,15 @@
 <?php
 require 'auth.php';
+require 'config.php';
 
-
+$prenotazione = $pdo->prepare("SELECT count(*) FROM (SELECT utente FROM locker UNION SELECT utente FROM queue) Presenza WHERE utente = ?");
+$prenotazione->execute([$_SESSION['user_id']]);
+$res = $prenotazione->fetchColumn();
+if ($res > 0) {
+    // L'utente ha già un armadietto assegnato
+    header("Location: myLocker.php");
+    exit();
+}
 
 
 $_SESSION['LockerBookingError'] = null;
--- userManager.php
+++ userManager.php
@@ -74,25 +74,9 @@ try {
                 $stmt = $pdo->prepare("INSERT INTO user (cf, email, password) VALUES (?, ?, ?)");
                 $stmt->execute([$username, $email, $password_hash]);
 
-                echo
-                <<<HTML
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <meta name="viewport" content="width=device-width,initial-scale=    1.0">
-                <link rel="preconnect" href="https://fonts.googleapis.com">
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-                <link rel="stylesheet" href="LockInStyle.css">
-            </head>
-            <body id="schermataHome">
-            <div id="schermata_registrazioneCompletata">
-                <h1>Registrazione andata a buon fine!</h1>
-                <button onclick="window.location.href='logIn.html'">Avanti e accedi</button>
-            </div>
-            </body>
-            <script> 
-            HTML;
+                header("Location: signUpSuccess.html");
+                
+           
             } catch (PDOException $e) {
                 // Gestione errori (es. duplicati)
                 if ($e->getCode() == 23000) {
